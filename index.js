a9c5940ef2263853c2a46adfd9350ea1d8458901
Deepak <deepakdeepu3025@gmail.com>
2024-05-07 19:19:47 +0530

    added a task and removed task

--- index.js
+++ index.js
@@ -1,77 +1,24 @@
-function binarySearch(arr, target) {
-  let left = 0;
-  let right = arr.length - 1;
+const fs = require('fs');
 
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
 
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+const data = 'Hello, this is some text to be written to the file!';
+
+const filePath = 'example.txt';
+
+
+fs.writeFile(filePath, data, (err) => {
+  if (err) {
+    console.error('Error creating the file:', err);
+    return;
   }
+  console.log(`File "${filePath}" has been created successfully!`);
+});
 
-  return -1;
-}
 
-function sumOfArray(arr) {
-  return arr.reduce((sum, num) => sum + num, 0);
-}
-
-function findMax(arr) {
-  return Math.max(...arr);
-}
-
-function findMin(arr) {
-  return Math.min(...arr);
-}
-
-function removeDuplicates(arr) {
-  return Array.from(new Set(arr));
-}
-
-function productOfArray(arr) {
-  return arr.reduce((product, num) => product * num, 1);
-}
-
-function isArraySorted(arr) {
-  return arr.every((num, index) => index === 0 || num >= arr[index - 1]);
-}
-
-function removeValueFromArray(arr, value) {
-  return arr.filter((num) => num !== value);
-}
-
-function calculateDifference(arr) {
-  let differences = [];
-  for (let i = 1; i < arr.length; i++) {
-    differences.push(arr[i] - arr[i - 1]);
+fs.unlink(filePath, (err) => {
+  if (err) {
+    console.error('Error deleting the file:', err);
+    return;
   }
-  return differences;
-}
-function countOccurrences(arr, target) {
-  let count = 0;
-  for (let num of arr) {
-    if (num === target) {
-      count++;
-    }
-  }
-  return count;
-}
-
-
-module.exports = {
-  binarySearch,
-  sumOfArray,
-  findMax,
-  findMin,
-  removeDuplicates,
-  productOfArray,
-  isArraySorted,
-  removeValueFromArray,
-  calculateDifference,
-  countOccurrences
-};
+  console.log(`File "${filePath}" has been deleted successfully!`);
+});
